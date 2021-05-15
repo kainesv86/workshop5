387bec316a6b4b07059b27a8bc9d49913818bec6
kainesv86 <kainesv86@gmail.com>
2021-05-16 04:17:08 +0700

    done FPT University

--- src/Dto/University.java
+++ src/Dto/University.java
@@ -9,6 +9,54 @@ package Dto;
  *
  * @author Kaine
  */
-public class University {
+public class University extends Organization{
+
+	private String name;
+
+	//Constructor
+	
+	public University() {
+		super();
+		this.name = "";
+	}
+
+	public University(int size, String name) {
+		super(size);
+		this.name = name;
+	}
+
+	
+	//Getter & setter
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	//Method
+
+	@Override
+	public void communicateByTool() {
+		System.out.println("In the university, people communicate by voice");
+	}
+
+	public void enroll() {
+		System.out.println("The registration for enrollment is only valid when the University has received all enrollment documents and enrollment fees");
+	}
+	
+	public void educate() {
+		System.out.println("Provide education at university standard");
+	}
+
+	public String toString() {
+		return ("encourage the advancement and development of knowledge");
+	}
 	
 }
+
+
+
+	
